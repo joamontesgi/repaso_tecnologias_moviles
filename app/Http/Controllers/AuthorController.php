81a00e7421082b91692ae211c4ad639c4b150721
joamontesgi <joamontesgi@unal.edu.co>
2024-07-25 11:27:22 -0500

    first commit

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Author;
+
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $authors = Author::all();
+        return response()->json($authors);
+        // return Author::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $author = new Author();
+        $author->name=$request->nombre;
+        $author->save();
+        return "Autor creado exitosamente";
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $author= Author::find($id);
+        $author->delete();
+        return "se elimino usuario";
+    }
+}
